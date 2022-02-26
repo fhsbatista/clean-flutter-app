e00a672884732ed93e3e341e13bc7531b56bd989
Fernando Batista <fernando.batista@ebanx.com>
2022-02-26 11:09:04 -0300

    ensure HttpAdapter calls post with correct values

---
+++ test/infra/http/http_adapter_test.dart
@@ -0,0 +1,39 @@
+import 'package:faker/faker.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:http/http.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import 'http_adapter_test.mocks.dart';
+
+class HttpAdapter {
+  final Client client;
+
+  HttpAdapter(this.client);
+
+  Future<void> request({required String url, required String method}) {
+    return client.post(Uri.parse(url));
+  }
+}
+
+@GenerateMocks([Client])
+void main() {
+  late HttpAdapter sut;
+  late MockClient client;
+
+  setUp(() {
+    client = MockClient();
+    sut = HttpAdapter(client);
+  });
+
+  group('post', () {
+    test('Should call post with correct values', () async {
+      when(client.post(any)).thenAnswer((_) async => Response('', 200));
+      final url = faker.internet.httpUrl();
+
+      await sut.request(url: url, method: 'post');
+
+      verify(client.post(Uri.parse(url)));
+    });
+  });
+}
