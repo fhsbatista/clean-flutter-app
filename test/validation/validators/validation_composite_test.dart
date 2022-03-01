fa566923be4c6d46ad61e030919aac7c4e529893
Fernando Batista <fernando.batista@ebanx.com>
2022-03-01 19:32:51 -0300

    move ValidationComposite setup to helper method

--- test/validation/validators/validation_composite_test.dart
+++ test/validation/validators/validation_composite_test.dart
@@ -20,14 +20,38 @@ class ValidationComposite implements Validation {
 
 @GenerateMocks([FieldValidation])
 void main() {
-  test('Should return null if all validations returns null or empty', () {
-    final validation1 = MockFieldValidation();
+  late ValidationComposite sut;
+  late MockFieldValidation validation1;
+  late MockFieldValidation validation2;
+  late MockFieldValidation validation3;
+
+  void mockValidation1(String? error) {
+    when(validation1.validate(any)).thenReturn(error);
+  }
+
+  void mockValidation2(String? error) {
+    when(validation2.validate(any)).thenReturn(error);
+  }
+
+  void mockValidation3(String? error) {
+    when(validation3.validate(any)).thenReturn(error);
+  }
+
+  setUp(() {
+    validation1 = MockFieldValidation();
+    validation2 = MockFieldValidation();
+    validation3 = MockFieldValidation();
     when(validation1.field).thenReturn('any field');
-    when(validation1.validate(any)).thenReturn(null);
-    final validation2 = MockFieldValidation();
     when(validation2.field).thenReturn('any field');
-    when(validation2.validate(any)).thenReturn('');
-    final sut = ValidationComposite([validation1, validation2]);
+    when(validation2.field).thenReturn('other field');
+    mockValidation1(null);
+    mockValidation2(null);
+    mockValidation3(null);
+    sut = ValidationComposite([validation1, validation2, validation3]);
+  });
+  
+  test('Should return null if all validations returns null or empty', () {
+    mockValidation2('');
 
     final error = sut.validate(field: 'any field', value: 'any value');
 
