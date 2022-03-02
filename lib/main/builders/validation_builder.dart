319eb4af3b7223a076b96625e7d00283d447ab10
Fernando Batista <fernando.batista@ebanx.com>
2022-03-01 21:04:06 -0300

    add ValidationBuilder

---
+++ lib/main/builders/validation_builder.dart
@@ -0,0 +1,28 @@
+import '../../validation/protocols/field_validation.dart';
+import '../../validation/validators/validators.dart';
+
+class ValidationBuilder {
+  static late ValidationBuilder _instance;
+  late String _fieldName;
+  List<FieldValidation> _validations = [];
+
+  ValidationBuilder._();
+
+  static ValidationBuilder field(String fieldName) {
+    _instance = ValidationBuilder._();
+    _instance._fieldName = fieldName;
+    return _instance;
+  }
+
+  ValidationBuilder required() {
+    _validations.add(RequiredFieldValidation(_fieldName));
+    return this;
+  }
+
+  ValidationBuilder email() {
+    _validations.add(EmailValidation(_fieldName));
+    return this;
+  }
+
+  List<FieldValidation> build() => _validations;
+}
--- lib/main/factories/pages/login/login_validation_factory.dart
+++ lib/main/factories/pages/login/login_validation_factory.dart
@@ -1,9 +1,10 @@
+import 'package:fordev/main/builders/validation_builder.dart';
 import 'package:fordev/validation/validators/validators.dart';
+import '../../../builders/builders.dart';
 
 ValidationComposite makeLoginValidation() {
   return ValidationComposite([
-    RequiredFieldValidation('email'),
-    EmailValidation('email'),
-    RequiredFieldValidation('password'),
+    ...ValidationBuilder.field('email').required().email().build(),
+    ...ValidationBuilder.field('password').required().build(),
   ]);
 }
