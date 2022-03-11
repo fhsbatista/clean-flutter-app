81044bf1dd1c5c90957447308cc07f672e3b1494
Fernando Batista <fernandohsbatista@gmail.com>
2022-03-11 19:38:02 -0300

    ensure RemoteAddACcount calls HttpClient with correct values

---
+++ lib/data/usecases/add_account/remote_add_account.dart
@@ -0,0 +1,53 @@
+import '../../../domain/entities/entities.dart';
+import '../../../domain/helpers/helpers.dart';
+import '../../../domain/usecases/add_account.dart';
+
+import '../../models/models.dart';
+import '../../http/http.dart';
+
+class RemoteAddAccount {
+  final HttpClient httpClient;
+  final String url;
+
+  RemoteAddAccount({
+    required this.httpClient,
+    required this.url,
+  });
+
+  Future<void> add(AddAccountParams params) async {
+    await httpClient.request(
+      url: url,
+      method: 'post',
+      body: RemoteAddAccountParams.fromDomain(params).toJson(),
+    );
+  }
+}
+
+class RemoteAddAccountParams extends AddAccountParams {
+  RemoteAddAccountParams({
+    required String name,
+    required String email,
+    required String password,
+    required String passwordConfirmation,
+  }) : super(
+          name: name,
+          email: email,
+          password: password,
+          passwordConfirmation: passwordConfirmation,
+        );
+
+  factory RemoteAddAccountParams.fromDomain(AddAccountParams params) =>
+      RemoteAddAccountParams(
+        name: params.name,
+        email: params.email,
+        password: params.password,
+        passwordConfirmation: params.passwordConfirmation,
+      );
+
+  Map toJson() => {
+        'name': name,
+        'email': email,
+        'password': password,
+        'passwordConfirmation': passwordConfirmation,
+      };
+}
---
+++ test/data/usecases/add_account/remote_add_account_test.dart
@@ -0,0 +1,64 @@
+import 'package:faker/faker.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import 'package:fordev/domain/usecases/add_account.dart';
+
+import 'package:fordev/data/http/http.dart';
+import 'package:fordev/data/usecases/usecases.dart';
+
+import 'remote_add_account_test.mocks.dart';
+
+@GenerateMocks([HttpClient])
+void main() {
+  late RemoteAddAccount sut;
+  late MockHttpClient httpClient;
+  late String url;
+  late AddAccountParams params;
+
+  Map getValidData() => {
+        'accessToken': faker.guid.guid(),
+        'name': faker.person.name(),
+      };
+
+  PostExpectation mockRequest() => when(
+        httpClient.request(
+          url: anyNamed('url'),
+          method: anyNamed('method'),
+          body: anyNamed('body'),
+        ),
+      );
+
+  void mockHttpData(Map data) => mockRequest().thenAnswer((_) async => data);
+
+  void mockHttpError(HttpError error) => mockRequest().thenThrow(error);
+
+  setUp(() {
+    httpClient = MockHttpClient();
+    url = faker.internet.httpUrl();
+    sut = RemoteAddAccount(httpClient: httpClient, url: url);
+    params = AddAccountParams(
+      name: faker.person.name(),
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+      passwordConfirmation: faker.internet.password(),
+    );
+    mockHttpData(getValidData());
+  });
+
+  test('Should call HttpClient with correct values', () async {
+    await sut.add(params);
+
+    verify(httpClient.request(
+      url: url,
+      method: 'post',
+      body: {
+        'name': params.name,
+        'email': params.email,
+        'password': params.password,
+        'passwordConfirmation': params.passwordConfirmation,
+      },
+    )).called(1);
+  });
+}
