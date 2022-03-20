9635ef93e60ef0e016cf157185abf03e12dd95bb
Fernando Batista <fernandohsbatista@gmail.com>
2022-03-20 11:34:56 -0300

    ensure HttpAdapter returns data on 200

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -237,5 +237,26 @@ void main() {
         },
       ));
     });
+
+    test('Should return data if get returns 200', () async {
+      final response = await sut.request(
+        url: faker.internet.httpUrl(),
+        method: 'get',
+      );
+
+      expect(response, {'any_key': 'any_value'});
+    });
+
+    test('Should return empty object if get returns 200 with no data',
+        () async {
+      mockResponse(200, body: '');
+
+      final response = await sut.request(
+        url: faker.internet.httpUrl(),
+        method: 'get',
+      );
+
+      expect(response, {});
+    });
   });
 }
