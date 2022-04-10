af83b8a9f19def643e7ff5904f896536da01d952
Fernando Batista <fernandohsbatista@gmail.com>
2022-04-10 14:46:52 -0300

    removes import

--- lib/ui/pages/surveys/surveys_page.dart
+++ lib/ui/pages/surveys/surveys_page.dart
@@ -1,6 +1,5 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 
 import '../../components/components.dart';
 import '../../helpers/i18n/i18n.dart';
