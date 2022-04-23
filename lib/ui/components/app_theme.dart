fd9a7637663d0bb9deebd24e9d148843fafb9584
Fernando Batista <fernandohsbatista@gmail.com>
2022-04-23 17:38:48 -0300

    add Survey Result layout

--- lib/ui/components/app_theme.dart
+++ lib/ui/components/app_theme.dart
@@ -4,12 +4,19 @@ ThemeData makeAppTheme() {
   final primaryColor = Color(0xFF7c4dff);
   final primaryDark = Color(0xFF3f1dcb);
   final primaryLight = Color(0xFFb47cff);
+  final secondaryColor = Color.fromRGBO(0, 77, 64, 1);
   final secondaryColorDark = Color.fromRGBO(0, 37, 26, 1);
+  final disabledColor = Colors.grey[400];
+  final dividerColor = Colors.grey;
   final theme = ThemeData(
+    backgroundColor: Colors.white,
     primaryColor: primaryColor,
     primaryColorDark: primaryDark,
     primaryColorLight: primaryLight,
     secondaryHeaderColor: secondaryColorDark,
+    highlightColor: secondaryColor,
+    disabledColor: disabledColor,
+    dividerColor: dividerColor,
     textTheme: TextTheme(
       headline1: TextStyle(
         fontSize: 30,
--- lib/ui/pages/survey_result/survey_result_page.dart
+++ lib/ui/pages/survey_result/survey_result_page.dart
@@ -7,7 +7,70 @@ class SurveyResultPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text(I18n.strings.surveys)),
-      body: Text('OK'),
+      body: ListView.builder(
+        itemCount: 4,
+        itemBuilder: (context, index) {
+          if (index == 0) {
+            return Container(
+              padding: EdgeInsets.only(
+                top: 40,
+                bottom: 20,
+                left: 20,
+                right: 20,
+              ),
+              decoration: BoxDecoration(
+                color: Theme.of(context).disabledColor.withAlpha(90),
+              ),
+              child: Text('Qual é o seu framework favorito?'),
+            );
+          }
+          return Column(
+            children: [
+              Container(
+                padding: const EdgeInsets.all(15),
+                decoration: BoxDecoration(
+                  color: Theme.of(context).backgroundColor,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Image.network(
+                      'https://cdn.smartworldclub.net/1387895/accesos_restringidos_web_registros_realizado_con_ruby_on_rails.jpg.webp',
+                      width: 80,
+                      height: 40,
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 10),
+                        child: Text(
+                          'Ruby on rails',
+                          style: TextStyle(fontSize: 16),
+                        ),
+                      ),
+                    ),
+                    Text(
+                      '100%',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: Theme.of(context).primaryColorDark,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 10),
+                      child: Icon(
+                        Icons.check_circle,
+                        color: Theme.of(context).highlightColor,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const Divider(height: 1),
+            ],
+          );
+        },
+      ),
     );
   }
 }
