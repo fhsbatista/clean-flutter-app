a5c97849d06154670b35dd7c94c7f71d98638562
Fernando Batista <fernandohsbatista@gmail.com>
2022-04-24 11:55:33 -0300

    extract widget for SurveyResult

--- lib/ui/pages/survey_result/survey_result_page.dart
+++ lib/ui/pages/survey_result/survey_result_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import '../../components/components.dart';
 import '../../helpers/i18n/i18n.dart';
 import '../pages.dart';
+import 'components/components.dart';
 
 class SurveyResultPage extends StatelessWidget {
   final SurveyResultPresenter? presenter;
@@ -34,71 +35,7 @@ class SurveyResultPage extends StatelessWidget {
               );
             }
             if (snapshot.hasData) {
-              return ListView.builder(
-                itemCount: 4,
-                itemBuilder: (context, index) {
-                  if (index == 0) {
-                    return Container(
-                      padding: EdgeInsets.only(
-                        top: 40,
-                        bottom: 20,
-                        left: 20,
-                        right: 20,
-                      ),
-                      decoration: BoxDecoration(
-                        color: Theme.of(context).disabledColor.withAlpha(90),
-                      ),
-                      child: Text('Qual é o seu framework favorito?'),
-                    );
-                  }
-                  return Column(
-                    children: [
-                      Container(
-                        padding: const EdgeInsets.all(15),
-                        decoration: BoxDecoration(
-                          color: Theme.of(context).backgroundColor,
-                        ),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Image.network(
-                              'https://cdn.smartworldclub.net/1387895/accesos_restringidos_web_registros_realizado_con_ruby_on_rails.jpg.webp',
-                              width: 80,
-                              height: 40,
-                            ),
-                            Expanded(
-                              child: Padding(
-                                padding:
-                                    const EdgeInsets.symmetric(horizontal: 10),
-                                child: Text(
-                                  'Ruby on rails',
-                                  style: TextStyle(fontSize: 16),
-                                ),
-                              ),
-                            ),
-                            Text(
-                              '100%',
-                              style: TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.bold,
-                                color: Theme.of(context).primaryColorDark,
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(left: 10),
-                              child: Icon(
-                                Icons.check_circle,
-                                color: Theme.of(context).highlightColor,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                      const Divider(height: 1),
-                    ],
-                  );
-                },
-              );
+              return SurveyResult();
             }
             return SizedBox.shrink();
           },
